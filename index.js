5ac75c920d2f8dca14a842b942d69a2c25c4b468
João-BenedetMachado <105388117+joaobenedetmachado@users.noreply.github.com>
2022-07-29 20:11:19 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function xx() {
+    var foto = document.getElementById("imagem")
+    foto.src = 'tenis2.png'
+}
+function yy() {
+    var foto = document.getElementById("imagem")
+    foto.src = 'seleção.png'
+}
+function zz() {
+    var foto = document.getElementById("imagem")
+    foto.src = 'tenis3.png'
+    
+}
